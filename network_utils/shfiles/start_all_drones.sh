2fdb909d1e80857bc7f21556819a9766687744db
gaoyong <gaoyong_chn@163.com>
2024-06-06 15:15:56 +0800

    change control way

---
+++ network_utils/shfiles/start_all_drones.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# 配置无人机 IP 地址、用户、密码等信息
+DRONE1_IP="192.168.1.1"
+DRONE2_IP="192.168.1.2"
+DRONE3_IP="192.168.1.3"
+DRONE4_IP="192.168.1.4"
+DRONE5_IP="192.168.1.5"
+USER="coolas"
+PASSWORD="504"
+NUM_DRONES=2
+BASE_PORT=5555
+OFFSET_MULTIPLIER=1.0
+ODOM_TOPIC="/vins_fusion/imu_propagate"
+VINS_TOPIC="/vins_fusion/imu_propagate"
+
+start_drone() {
+    DRONE_ID=$1
+    sshpass -p $PASSWORD ssh $USER@$2 "bash -c '~/JKW_PROJECT/swarm_vins_ws/src/swarm_vins/network_utils/shfiles/start_drone.sh $DRONE_ID $NUM_DRONES $DRONE1_IP $DRONE2_IP $DRONE3_IP $DRONE4_IP $DRONE5_IP $BASE_PORT $OFFSET_MULTIPLIER ${ODOM_TOPIC/X/$DRONE_ID} $VINS_TOPIC'"
+}
+
+# 启动无人机
+start_drone 1 $DRONE1_IP
+start_drone 2 $DRONE2_IP
+start_drone 3 $DRONE3_IP
+start_drone 4 $DRONE4_IP
+start_drone 5 $DRONE5_IP
---
+++ network_utils/shfiles/start_drone.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Usage: ./start_drone.sh <drone_id> <num_drones> <drone1_ip> <drone2_ip> <drone3_ip> <drone4_ip> <drone5_ip> <base_port> <offset_multiplier> <odom_topic> <vins_topic>
+
+DRONE_ID=$1
+NUM_DRONES=$2
+DRONE1_IP=$3
+DRONE2_IP=$4
+DRONE3_IP=$5
+DRONE4_IP=$6
+DRONE5_IP=$7
+BASE_PORT=$8
+OFFSET_MULTIPLIER=$9
+ODOM_TOPIC=${10}
+VINS_TOPIC=${11}
+
+roslaunch my_package start_drone.launch \
+    drone_id:=$DRONE_ID \
+    num_drones:=$NUM_DRONES \
+    drone1_ip:=$DRONE1_IP \
+    drone2_ip:=$DRONE2_IP \
+    drone3_ip:=$DRONE3_IP \
+    drone4_ip:=$DRONE4_IP \
+    drone5_ip:=$DRONE5_IP \
+    base_port:=$BASE_PORT \
+    offset_multiplier:=$OFFSET_MULTIPLIER \
+    odom_topic:=$ODOM_TOPIC \
+    vins_topic:=$VINS_TOPIC
