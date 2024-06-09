59984f90e3ba8407150230901b825f495fc3ad14
gaoyong <gaoyong_chn@163.com>
2024-06-09 15:38:23 +0800

    fix a small problem

--- network_utils/shfiles/tmp/start_drone_node.sh
+++ network_utils/shfiles/tmp/start_drone_node.sh
@@ -4,8 +4,9 @@
 DRONE_ID=$1
 SERVER_IP=$2
 SERVER_PORT=$3
+OFFSET_MULTIPLIER=$4
 
 source /opt/ros/noetic/setup.bash;
 source ~/JKW_PROJECT/swarm_vins_ws/devel/setup.bash;
 
-roslaunch network_utils drone_node.launch drone_id:=$DRONE_ID server_ip:=$SERVER_IP server_port:=$SERVER_PORT;
+roslaunch network_utils drone_node.launch drone_id:=$DRONE_ID server_ip:=$SERVER_IP server_port:=$SERVER_PORT offset_multiplier:=$OFFSET_MULTIPLIER;
