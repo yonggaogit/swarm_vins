31b55f03df93d60f98e48ace4fb5557c8d651f71
gao <gaoyong_chn@163.com>
2024-06-09 15:54:07 +0800

    fix a problem

--- network_utils/shfiles/2_start_rviz.sh
+++ network_utils/shfiles/2_start_rviz.sh
@@ -6,4 +6,4 @@ source ~/JKW_PROJECT/swarm_vins_ws/src/swarm_vins/network_utils/config/drone_con
 source /opt/ros/noetic/setup.bash;
 source ~/JKW_PROJECT/swarm_vins_ws/devel/setup.bash;
 
-roslaunch network_utils server_node.launch server_ip:=$SERVER_IP server_port:=CLIENT_TO_SERVER_PORT rviz_config:=RVIZ_CONFIG;
+roslaunch network_utils server_node.launch server_ip:=$SERVER_IP server_port:=$CLIENT_TO_SERVER_PORT rviz_config:=$RVIZ_CONFIG;
